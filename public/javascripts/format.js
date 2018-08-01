c0bb336b5d3740f73ccb353c7248d69f1f7887af
zhuangrong2025 <42589963@qq.com>
2018-08-01 15:10:08 +0800

    入标题123-高亮选中插入的文字

--- public/javascripts/format.js
+++ public/javascripts/format.js
@@ -1,7 +1,7 @@
 (function($){
 	// 获取url参数
-	$.format = function(character){
-		console.log(character)
+	$.format = function(){
+
 	}
 	// 加粗 斜体 等前后字符包裹文本
 	$.format.wraper = function(val, el, character, startPos, endPos, startText, endText, selectedText){
@@ -14,7 +14,11 @@
          el.selectionEnd = endPos - c_len
        }, 10)
     }else{
-      content = startText + character + selectedText + character + endText
+      if(character == "# " || character == "## " || character == "### " ){
+        content = startText + character + selectedText + endText
+      }else{
+        content = startText + character + selectedText + character + endText
+      }
       setTimeout(()=>{
         el.selectionStart = startPos + c_len
         el.selectionEnd = endPos + c_len
@@ -22,4 +26,19 @@
     }
 		return content
 	}
+  // 插入格式
+  $.format.insert = function(el, character, startPos, endPos, startText, endText, presetText){
+    var pre_len = presetText.length
+    var c_len = character.length
+    content =  startText + presetText + endText
+    setTimeout(()=>{
+      if( character == "**" || character == "_"){
+        el.setSelectionRange(startPos + c_len, startPos + (pre_len - c_len))
+      }else{
+        el.setSelectionRange(startPos + c_len, startPos + pre_len)
+      }
+    }, 10)
+
+    return content
+  }
 })(jQuery)
